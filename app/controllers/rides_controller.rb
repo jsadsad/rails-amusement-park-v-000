827bc206ebdf2e6e3f89bdb262d784326117dc7f
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-10-01 19:12:38 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,4 +1,6 @@
 class RidesController < ApplicationController
   def create
-    
+    ride = Ride.create(ride_params)
+    message = ride.take_ride
+    redirect to user_path(ride.user), flash: {message: message}
 end
