7c4247715190e0ae7fca0fe02b326e1d0c75833c
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-10-01 20:08:39 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,6 +4,15 @@ class Ride < ActiveRecord::Base
 
   def take_ride
     user_has_enough_tickets, user_is_tall_enough = user_meets_requirements
+    if user_has_enough_tickets && user_is_tall_enough
+      start_ride
+    elsif user_is_tall_enough && !user_has_enough_tickets
+      "Sorry! " + not_enough_tickets
+    elsif !user_is_tall_enough && user_has_enough_tickets
+      "Sorry! " + not_tall_enough
+    elsif !user_is_tall_enough && !user_has_enough_tickets
+      "Sorry! " + not_tall_enough + " " + not_enough_tickets
+    end 
   end
 
   def user_meets_requirements
